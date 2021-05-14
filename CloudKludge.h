90f5ce148d3712b7cfc100e76f09d6e4865f3fdd
Fast Reports <support@fast-report.com>
2021-05-14 13:40:46 +0300

    sync 2021-05-14

---
+++ CloudKludge.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+
+namespace fastreport {
+	namespace cloud {
+		namespace models {
+
+			struct DefaultEnum {
+				int32_t _0;
+			};
+
+			DefaultEnum TypeEnum;
+			DefaultEnum LevelEnum;
+			DefaultEnum CreateEnum;
+			DefaultEnum R_deleteEnum;
+			DefaultEnum ExecuteEnum;
+			DefaultEnum GetEnum;
+			DefaultEnum UpdateEnum;
+			DefaultEnum AdministrateEnum;
+		}
+	}
+}
